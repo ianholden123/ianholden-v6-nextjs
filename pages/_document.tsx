fb7dcea41ec3072388f754db6962c5e1df4964c9
Ian Holden <ianholden123@hotmail.co.uk>
2023-01-29 13:59:17 +0000

    Noindex the webiste.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
           content="Ian Holden is a Software Engineer based in Kent, UK. Get in touch to see how Ian can help you build your next web project."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="noindex" />
         <Favicons />
       </Head>
       <body>
