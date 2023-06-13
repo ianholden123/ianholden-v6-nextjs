045331fe8ffbc27d254671e6662e149767c95737
Ian Holden <ianholden123@hotmail.co.uk>
2023-06-13 21:32:50 +0100

    Integrated Statsig via Rudderstack. Implemented page view tracking in Rudderstack.

---
+++ lib/Services/Rudderstack/Rudderstack.ts
@@ -0,0 +1,20 @@
+import Cookies from "js-cookie";
+import constants from "@/lib/Constants";
+import { TTrackPage, TIsRudderStackAvailable } from "./Rudderstack.types";
+
+// Middleware will automatically set a cookie for the user if they visit a page
+const sessionUUID = Cookies.get(constants.uuid_cookie);
+
+const commonProperties = {
+  statsigCustomIDs: ["sessionUUID", sessionUUID],
+  statsigEnvironment: { tier: process.env.NODE_ENV },
+};
+
+export const isRudderStackAvailable: TIsRudderStackAvailable = () =>
+  typeof window !== "undefined" &&
+  typeof window.rudderanalytics !== "undefined";
+
+export const trackPageView: TTrackPage = (name, properties) => {
+  if (!isRudderStackAvailable()) return;
+  window.rudderanalytics?.page(name, { commonProperties, ...properties });
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
+import Cookies from "js-cookie";
+import { StatsigProvider } from "statsig-react";
 import "@/styles/globals.css";
+import constants from "@/lib/Constants";
+import { usePageViewTracking } from "@/lib/Hooks/usePageViewTracking";
 import { Scripts } from "@/components/_miscellaneous/scripts";
 import { SiteNavigation } from "@/components/organisms/SiteNavigation";
 import { Footer } from "@/components/organisms/Footer";
 
 export default function App({ Component, pageProps }) {
+  // Middleware will automatically set a cookie for the user if they visit a page
+  const sessionUUID = Cookies.get(constants.uuid_cookie);
+
+  usePageViewTracking();
+
   return (
-    <>
+    <StatsigProvider
+      sdkKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_API_KEY}
+      waitForInitialization={true}
+      user={{
+        customIDs: {
+          sessionUUID,
+        },
+      }}
+      options={{
+        environment: { tier: process.env.NODE_ENV },
+      }}
+    >
       <Scripts />
       <SiteNavigation>
         <main>
@@ -13,6 +33,6 @@ export default function App({ Component, pageProps }) {
         </main>
         <Footer />
       </SiteNavigation>
-    </>
+    </StatsigProvider>
   );
 }
