413867a2040b5ab31f0547834f9d38ad1e7271e8
Ian Holden <ianholden123@hotmail.co.uk>
2023-06-18 21:15:54 +0100

    Updated styles. Added Contact page.

---
+++ components/templates/Contact/Contact.tsx
@@ -0,0 +1,23 @@
+export const ContactTemplate = () => (
+  <div className="prose container py-10">
+    <h1>Contact Ian</h1>
+    <p>
+      If you would like to work with me on a project, I would be more than happy
+      to help! Get in touch by using one of the following channels below:
+    </p>
+    <a
+      href="mailto:ianholdendev@outlook.com"
+      className="btn btn-neutral btn-wide mr-5"
+    >
+      Email
+    </a>
+    <a
+      href="https://www.linkedin.com/in/ianlewisholden"
+      className="btn btn-neutral btn-wide"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      LinkedIn
+    </a>
+  </div>
+);
